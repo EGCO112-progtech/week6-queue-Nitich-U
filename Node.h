d119f0a274f6fc2c7cc0b25bb454ced8401fa7c8
Nitich-U <nitich.uan@student.mahidol.edu>
2025-02-13 17:57:38 +0000

    initial commit

--- Node.h
+++ Node.h
@@ -4,11 +4,10 @@
 //
 //  Created by Mingmanas Sivaraksa on 5/2/2566 BE.
 //
-#ifndef Node_h
-#define Node_h
+#ifndef NODE_H
+#define NODE_H
 
-struct node
-{
+struct node {
     int data;
     struct node *nextPtr;
 };
@@ -16,28 +15,32 @@ struct node
 typedef struct node Node;
 typedef struct node* NodePtr;
 
-
-void enqueue(NodePtr * head, NodePtr* tail, int x){
-  Node* new_node=(NodePtr) malloc(sizeof(Node));
-if(new_node){ 
-    /* Finish queue*/
- }
+void enqueue(NodePtr *head, NodePtr *tail, int x) {
+    Node* new_node = (NodePtr)malloc(sizeof(Node));
+    if (new_node) { 
+        new_node->data = x;
+        new_node->nextPtr = NULL;
+        if (*head == NULL) 
+            *head = new_node;
+        else 
+            (*tail)->nextPtr = new_node;              
+        *tail = new_node;
+    }
 }
 
-
-int dequeue(NodePtr* head, NodePtr* tail){
-  NodePtr t=*head;
-   if(t){
-   int value= t->data;
-   /* Finish dequeue*/
-       
-       
-   return value;
-   }
-   printf("Empty queue");
-   return 0;
+int dequeue(NodePtr *head, NodePtr *tail) {
+    if (*head == NULL) {
+        printf("Empty queue\n");
+        return 0;
+    }
+    
+    NodePtr t = *head;
+    int value = t->data;
+    *head = t->nextPtr;
+    if (*head == NULL) 
+        *tail = NULL;
+    free(t);
+    return value;
 }
-
-
 
 #endif
