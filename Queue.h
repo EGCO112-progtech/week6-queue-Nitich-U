be47d2c552cfde10c85a95785545db3c8a60a289
Nitich-U <nitich.uan@student.mahidol.edu>
2025-02-13 17:58:24 +0000

    initial commit

--- Queue.h
+++ Queue.h
@@ -1,27 +1,44 @@
 
 
 typedef struct {
-	 NodePtr headPtr,tailPtr;
-	int size;
-}Queue;
+  NodePtr headPtr, tailPtr;
+  int size;
+} Queue;
 
-
-void enqueue_struct(Queue* q, int x){
-  Node *new_node=(Node*) malloc(sizeof(Node));
-if(new_node){ 
-  /*Finish enqueue */
- }
+void enqueue_struct(Queue* q, int x) {
+  NodePtr new_node = (NodePtr)malloc(sizeof(Node));
+  if (new_node) {
+      new_node->data = x;
+      new_node->nextPtr = NULL;
+      
+      if (q->size == 0) { // node แรก
+          q->headPtr = new_node;
+      } else { // node อื่นๆ
+          q->tailPtr->nextPtr = new_node;
+      }
+      
+      q->tailPtr = new_node;
+      q->size++;
+  }
 }
 
-
-int dequeue_struct(Queue *q){
-   NodePtr t=q->headPtr;
-   if(t){
-   int value= t->data;
-       /*Finish dequeue */
-   return value;
-   }
-   printf("Empty queue");
-   return 0;
+int dequeue_struct(Queue* q) {
+  if (q->size == 0) {
+      printf("Empty queue\n");
+      return 0;
+  }
+  
+  NodePtr t = q->headPtr;
+  int value = t->data;
+  q->headPtr = t->nextPtr;
+  free(t);
+  
+  q->size--;
+  if (q->size == 0) {
+      q->tailPtr = NULL;
+  }
+  
+  return value;
 }
+
 
