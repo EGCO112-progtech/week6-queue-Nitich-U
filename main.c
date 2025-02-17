10af0b4899b9af8bd0b0f3dc2aae42f81c5837dc
Nitich-U <nitich.uan@student.mahidol.edu>
2025-02-17 06:47:32 +0000

    main

--- main.c
+++ main.c
@@ -2,26 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Node.h"
+
 #include "Queue.h"
 
-int main(int argc, char **argv) {
-    Queue q;
-    q.headPtr = NULL;
-    q.tailPtr = NULL;
-    q.size = 0;
+int main(int argc , char **argv) {
+  NodePtr headPtr=NULL;
+   NodePtr tailPtr=NULL;
+  Queue  q;
+   q. headPtr=NULL;
+   q.tailPtr=NULL;
+   q.size=0;
 
-    int i, x;
 
-    for (i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "x") == 0) {
-            x = dequeue_struct(&q);
-            if (q.size > 0) {
-                printf("Dequeuing %d\n", x);
+   int i,x;
+   
+
+ for(i=1;i<argc;i++){
+        if(strcmp(argv[i],"x")==0){
+            if(q.size!=0 ) {
+               x=dequeue_struct(&q);
+               printf("dequeing %d\n",x);
             }
-        } else {
-            enqueue_struct(&q, atoi(argv[i]));
+            else printf("Empty queue\n");
         }
-    }
-    return 0;
+        else {
+       enqueue_struct(&q, atoi(argv[i]));
+          
+        }
+ }
+  return 0;
 }
-
